e83d7bfed9b569b17349cb91e4d437a8a286d2ef
wuyangtang <wuyangtang@live.com>
2017-12-10 14:03:37 +0800

    Auto generation for latest news

---
+++ js/header.js
@@ -0,0 +1,8 @@
+(function ($) {
+    $(function () {
+        // Highlight navbar for current page
+        $('li.nav-item').each((index, item) => {
+            console.log('TODO highlight navbar for current page');
+        });
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
